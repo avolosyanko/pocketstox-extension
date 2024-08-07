6e5828485d0f65f852b54a4b6176b224eb5eeea5
avolosyanko <avolosyanko@general-index.com>
2024-08-07 20:57:26 +0100

    file restructuring changes

---
+++ Extension/lambda/DataHandler.py
@@ -0,0 +1,16 @@
+import logging
+
+log = logging.Logger(__name__)
+
+class DataHandler:
+    '''
+    Add __init__() function
+    '''
+    def recieve_api_data():
+        return None
+    
+    def send_api_data():
+        return None
+    
+    def search_vectorstore():
+        return None
