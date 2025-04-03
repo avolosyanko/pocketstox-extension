9da3b203101f31e541958a3d43a6743b41f605dc
avolosyanko <andres.volosyanko@gmail.com>
2025-04-03 12:53:59 +0100

    Readability.min.js added locally, scripts updated to parse article and return in console on click

--- Extension/scripts/popup.js
+++ Extension/scripts/popup.js
@@ -1,26 +1,34 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'contentExtracted') {
-        console.log('Content extracted:', message);
-        // Handle article contents
-        // Send to API Gateway.   
+console.log('popup.js loaded');
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const scrapeButton = document.getElementById('scrape');
+    if (scrapeButton) {
+        scrapeButton.addEventListener('click', () => {
+            console.log('popup.js scrape button clicked!');
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.scripting.executeScript({
+                    target: {tabId: tabs[0].id},
+                    files: ['scripts/Readability.min.js']
+                }).then(() => {
+                    chrome.scripting.executeScript({
+                        target: {tabId: tabs[0].id},
+                        files: ['scripts/content.js']
+                    });
+                });
+            });
+        });
+    } else {
+        console.error('Button with id "scrape" not found');
     }
-});
 
-
-
-
-
-
-document.getElementById('scrape').addEventListener('click', () => {
-    chrome.tabs.executeScript({
-        file: 'content.js'
+    chrome.runtime.onMessage.addListener((message) => {
+        if (message.action === 'contentExtracted') {
+            const title = message.title;
+            const content = message.content;
+            
+            console.log('Title:', title);
+            console.log('Content:', content); 
+        }
     });
 });
-
-chrome.runtime.onMessage.addListener((message) => {
-    if (message.action === 'contentExtracted') {
-        document.getElementById('output').textContent = message.content;
-    }
-});
-
-
